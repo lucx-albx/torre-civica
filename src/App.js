2d90cc3e6fa4629d8ec39a1540405847200efdf5
lucx-albx <117486221+lucx-albx@users.noreply.github.com>
2023-01-08 16:49:26 +0100

    Progetto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Navbar from './NavBar.js'
+import Footer from './Footer.js'
+
+const App = () => {
+	return (
+		<div className='container-fluid'>
+			<Navbar/>
+			<Footer/>
+		</div>
+	)
+}
+
+export default App
